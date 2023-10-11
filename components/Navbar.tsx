f2e2ee18a05dab593d6ab284a572fe89f773e9df
Lydia Kim <lkim3@scu.edu>
2023-10-10 22:13:03 -0700

    initial setup

---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <div>
+      <div className="w-screen">
+        <h1 className="pl-4 pt-4 text-3xl">CheckMate</h1>
+
+      </div>
+    </div>
+  );
+}
