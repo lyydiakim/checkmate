e8f89924bd35645d2eb15a066cb993341d2fc50b
Lydia Kim <lkim3@scu.edu>
2023-10-19 11:35:35 -0700

    new webpage added

--- app/landing-page/OurService.tsx
+++ app/landing-page/OurService.tsx
@@ -1,10 +1,13 @@
 "use client";
+import Link from "next/link";
 export default function OurService() {
   return (
     <div className="flex flex-row text-white">
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
       <div className=" px-8 py-4 text-xl flex justify justify-center ">
         <p className="underline-offset-4 decoration-white">Our Service</p>
+
+        <Link href="../split-page">Testing Link to Split Page</Link>
       </div>
     </div>
   );
