aa4b85f32cd97c6b9a06f6a57fc4136434bdb335
Morgan <mwatts@scu.edu>
2023-10-26 20:59:15 -0700

    removed "Our Service"
    Removed "Our Service" from the homepage, although the file for it is still active. Removed it to avoid redundancy with the "About Us" section and to improve the appearance of the homepage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import UploadImage from "@/components/landing-page/UploadImage";
-import OurService from "@/components/landing-page/OurService";
 
 export default function Home() {
   return (
     <div className="">
       <UploadImage />
-      <OurService />
     </div>
   );
 }
