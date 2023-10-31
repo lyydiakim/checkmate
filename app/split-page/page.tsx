8065836e36cc3d8e5a651110ff6b71357c09efac
Lydia Kim <lkim3@scu.edu>
2023-10-31 11:29:48 -0700

    update split page

--- app/split-page/page.tsx
+++ app/split-page/page.tsx
@@ -35,32 +35,49 @@ export default function page() {
     "
     >
       <div className="text-2xl mr-[5rem]">
-        <p className="mb-4">
+        {/* <p className="mb-4">
           Your Uploaded Image <BsImage className="inline" />
-        </p>
-        <Image className=" w-96" src={receipt} alt="receipt" />
+        </p> */}
+        <Image className="w-auto h-[70%]" src={receipt} alt="receipt" />
       </div>
 
-      <div className="bg-gray-300 bg-opacity-20 rounded-lg text-2xl h-[70%] w-[50%]">
-        <p className="text-[#c6f3f5] text-3xl  text-bold p-10 ">
+      <div className="bg-gray-300 bg-opacity-20 rounded-lg  h-[70%] w-[50%]">
+        <p className="text-[#c6f3f5] text-[2.5rem]  text-bold p-10 ">
           How many people are splitting this receipt?
         </p>
+        <div className="text-gray-200 text-3xl flex flex-col space-y-[2rem]">
+          <div className=" pl-[5rem] ">
+            <p className="mt-1">
+              {" "}
+              <PiUserCircleFill
+                size={45}
+                className="inline text-blue-500 mr-4"
+              />
+              Meesh Af
+            </p>
+          </div>
 
-        <div className="flex flex-row pl-[5rem] pb-[2rem] ">
-          <PiUserCircleFill size={45} className="text-blue-500 mr-4" />
-          <p className="mt-1">Meesh Af</p>
+          <div className="pl-[5rem] ">
+            <p className="mt-1">
+              <PiUserCircleFill
+                size={45}
+                className="inline text-green-500 mr-4"
+              />
+              Ice Spice Af
+            </p>
+          </div>
+          <div className="pl-[5rem]">
+            <p className="mt-1">
+              <PiUserCircleFill
+                size={45}
+                className="inline text-pink-500 mr-4"
+              />
+              Princess Af
+            </p>
+          </div>
         </div>
 
-        <div className="flex flex-row pl-[5rem] pb-[2rem] ">
-          <PiUserCircleFill size={45} className="text-green-500 mr-4" />
-          <p className="mt-1">Ice Spice Af</p>
-        </div>
-        <div className="flex flex-row pl-[5rem] pb-[2rem]">
-          <PiUserCircleFill size={45} className="text-pink-500 mr-4" />
-          <p className="mt-1">Princess Af</p>
-        </div>
-
-        <div className="ml-[100px] mt-[9rem]">
+        <div className="pl-[5.5rem] mt-[9rem]">
           <Link
             className="bg-[#289AA1] border-2 border-[#9acbce]  border-solid hover:border-dotted text-2xl p-2 rounded-md "
             href="../share-page"
