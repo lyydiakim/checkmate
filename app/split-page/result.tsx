4131c9e303ac3df5d089866f0fec6ad370cedcf1
Lydia Kim <lkim3@scu.edu>
2023-11-07 21:19:42 -0800

    minor fixes

---
+++ app/split-page/result.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+// import { useRouter } from "next/navigation";
+
+// const ResultPage: React.FC = () => {
+//   const router = useRouter();
+//   const { numberOfPeople, names } = router.query;
+
+//   return (
+//     <div>
+//       <h1>Result Page</h1>
+//       <p>Number of People: {numberOfPeople}</p>
+//       <p>Names:</p>
+//       <ul>
+//         {Array.from({ length: Number(numberOfPeople) }, (_, index) => (
+//           <li key={index}>{names[index]}</li>
+//         )}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default ResultPage;
